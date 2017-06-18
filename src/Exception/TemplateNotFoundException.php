583a4886611cf9d1a21e3faf7bc24ed4e0ed793a
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2017-06-18 14:33:27 +0200

    composer and license

--- src/Exception/TemplateNotFoundException.php
+++ src/Exception/TemplateNotFoundException.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the `starychfojtu/viewcomponent` project.
+ *
+ * (c) https://github.com/starychfojtu/ViewComponentBundle/graphs/contributors
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace ViewComponent\Exception;
