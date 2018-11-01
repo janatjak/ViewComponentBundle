6fc0e1066d8d20f6d7bd624c589a7cd86bd9bc2c
Jakub Janata <jakubjanata@gmail.com>
2018-11-01 07:53:15 +0100

    upgrade symfony 4

--- Tests/Finder/ViewComponentFinderTest.php
+++ Tests/Finder/ViewComponentFinderTest.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Starychfojtu\ViewComponentBundle\Tests\Finder;
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
-class ViewComponentFinderTest extends PHPUnit_Framework_TestCase
+class ViewComponentFinderTest extends TestCase
 {
 
 }
